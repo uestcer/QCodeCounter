9da5993717092498624e26580675a0be35d0b151
liyiwei <leeyiw@gmail.com>
2014-02-03 23:59:39 +0800

    add dir choose table and view

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -3,6 +3,42 @@
 MainWindow::MainWindow(QWidget *parent)
 	: QMainWindow(parent)
 {
+	/* 设置窗口标题 */
+	this->setWindowTitle(tr("QCodeCounter"));
+
+	/* 设置中心控件 */
+	widget = new QWidget(this);
+	this->setCentralWidget(widget);
+
+	/* 初始化整体布局 */
+	mainLayout = new QHBoxLayout();
+	leftLayout = new QVBoxLayout();
+	mainLayout->addLayout(leftLayout);
+
+	/* 设置菜单栏 */
+	QMenu *menu;
+	menuBar = new QMenuBar(this);
+	menu = menuBar->addMenu(tr("File"));
+	mainLayout->setMenuBar(menuBar);
+
+	/* 初始化左侧布局 */
+	dirTableView = new QTableView();
+	dirAddBtn = new QPushButton(tr("Add"));
+	dirRemoveBtn = new QPushButton(tr("Remove"));
+	dirOpLayout = new QHBoxLayout();
+	dirOpLayout->addWidget(dirAddBtn);
+	dirOpLayout->addWidget(dirRemoveBtn);
+	dirLayout = new QVBoxLayout();
+	dirLayout->addWidget(dirTableView);
+	dirLayout->addLayout(dirOpLayout);
+	dirBox = new QGroupBox(tr("Code directory"));
+	dirBox->setLayout(dirLayout);
+
+	fileTypeBox = new QGroupBox(tr("File type"));
+	leftLayout->addWidget(dirBox);
+	leftLayout->addWidget(fileTypeBox);
+
+	this->centralWidget()->setLayout(mainLayout);
 }
 
 MainWindow::~MainWindow()
--- MainWindow.h
+++ MainWindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QMenuBar>
+#include <QGroupBox>
+#include <QTableView>
+#include <QPushButton>
 
 class MainWindow : public QMainWindow
 {
@@ -10,6 +16,18 @@ class MainWindow : public QMainWindow
 public:
 	MainWindow(QWidget *parent = 0);
 	~MainWindow();
+
+private:
+	QWidget *widget;
+	QHBoxLayout *mainLayout;
+	QMenuBar *menuBar;
+
+	/* 左侧布局使用的控件 */
+	QVBoxLayout *leftLayout, *dirLayout;
+	QHBoxLayout *dirOpLayout;
+	QGroupBox *dirBox, *fileTypeBox;
+	QTableView *dirTableView, *fileTypeTableView;
+	QPushButton *dirAddBtn, *dirRemoveBtn;
 };
 
 #endif // MAINWINDOW_H
