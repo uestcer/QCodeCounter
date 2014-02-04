e34eabf33e7c4f3589635dea1773eeea0bf38458
liyiwei <leeyiw@gmail.com>
2014-02-04 15:25:28 +0800

    add right result table view and button

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,3 +1,6 @@
+#include <QFileDialog>
+#include <QList>
+
 #include "MainWindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
@@ -6,14 +9,16 @@ MainWindow::MainWindow(QWidget *parent)
 	/* 设置窗口标题 */
 	this->setWindowTitle(tr("QCodeCounter"));
 
-	/* 设置中心控件 */
+	/* 初始化整体布局 */
+	this->setMinimumSize(800, 600);
 	widget = new QWidget(this);
 	this->setCentralWidget(widget);
-
-	/* 初始化整体布局 */
 	mainLayout = new QHBoxLayout();
 	leftLayout = new QVBoxLayout();
+	rightLayout = new QVBoxLayout();
 	mainLayout->addLayout(leftLayout);
+	mainLayout->addLayout(rightLayout);
+	this->centralWidget()->setLayout(mainLayout);
 
 	/* 设置菜单栏 */
 	QMenu *menu;
@@ -22,8 +27,16 @@ MainWindow::MainWindow(QWidget *parent)
 	mainLayout->setMenuBar(menuBar);
 
 	/* 初始化左侧布局 */
+	dirModel = new QStandardItemModel(0, 3);
+	dirModel->setHeaderData(0, Qt::Horizontal, tr("Directory"));
+	dirModel->setHeaderData(1, Qt::Horizontal, tr("Type"));
+	dirModel->setHeaderData(2, Qt::Horizontal, tr("Recursive"));
 	dirTableView = new QTableView();
+	dirTableView->setModel(dirModel);
+	dirTableView->verticalHeader()->setVisible(false);
+	dirTableView->setShowGrid(false);
 	dirAddBtn = new QPushButton(tr("Add"));
+	connect(dirAddBtn, SIGNAL(clicked()), this, SLOT(dirAddBtnClicked()));
 	dirRemoveBtn = new QPushButton(tr("Remove"));
 	dirOpLayout = new QHBoxLayout();
 	dirOpLayout->addWidget(dirAddBtn);
@@ -34,14 +47,52 @@ MainWindow::MainWindow(QWidget *parent)
 	dirBox = new QGroupBox(tr("Code directory"));
 	dirBox->setLayout(dirLayout);
 
+	fileTypeTableView = new QTableView();
+	fileTypeAddBtn = new QPushButton(tr("Add"));
+	fileTypeRemoveBtn = new QPushButton(tr("Remove"));
+	fileTypeOpLayout = new QHBoxLayout();
+	fileTypeOpLayout->addWidget(fileTypeAddBtn);
+	fileTypeOpLayout->addWidget(fileTypeRemoveBtn);
+	fileTypeLayout = new QVBoxLayout();
+	fileTypeLayout->addWidget(fileTypeTableView);
+	fileTypeLayout->addLayout(fileTypeOpLayout);
 	fileTypeBox = new QGroupBox(tr("File type"));
+	fileTypeBox->setLayout(fileTypeLayout);
+
 	leftLayout->addWidget(dirBox);
 	leftLayout->addWidget(fileTypeBox);
 
-	this->centralWidget()->setLayout(mainLayout);
+	/* 初始化右侧布局 */
+	resultModel = new QStandardItemModel(0, 4);
+	resultModel->setHeaderData(0, Qt::Horizontal, tr("File"));
+	resultTableView = new QTableView();
+	resultTableView->setModel(resultModel);
+	startBtn = new QPushButton(tr("Start"));
+	resultOpLayout = new QHBoxLayout();
+	resultOpLayout->addWidget(startBtn);
+	resultLayout = new QVBoxLayout();
+	resultLayout->addWidget(resultTableView);
+	resultLayout->addLayout(resultOpLayout);
+	resultBox = new QGroupBox(tr("Result"));
+	resultBox->setLayout(resultLayout);
+
+	rightLayout->addWidget(resultBox);
 }
 
 MainWindow::~MainWindow()
 {
 
+}
+
+void
+MainWindow::dirAddBtnClicked()
+{
+	QString dir = QFileDialog::getExistingDirectory(this);
+	if (dir != "") {
+		QList<QStandardItem *> items;
+		items << new QStandardItem(dir);
+		items << new QStandardItem("+");
+		items << new QStandardItem(tr("Yes"));
+		dirModel->appendRow(items);
+	}
 }
--- MainWindow.h
+++ MainWindow.h
@@ -7,6 +7,8 @@
 #include <QMenuBar>
 #include <QGroupBox>
 #include <QTableView>
+#include <QHeaderView>
+#include <QStandardItemModel>
 #include <QPushButton>
 
 class MainWindow : public QMainWindow
@@ -18,16 +20,29 @@ public:
 	~MainWindow();
 
 private:
+	/* 整体布局使用的控件 */
 	QWidget *widget;
 	QHBoxLayout *mainLayout;
 	QMenuBar *menuBar;
 
 	/* 左侧布局使用的控件 */
-	QVBoxLayout *leftLayout, *dirLayout;
-	QHBoxLayout *dirOpLayout;
+	QVBoxLayout *leftLayout, *dirLayout, *fileTypeLayout;
+	QHBoxLayout *dirOpLayout, *fileTypeOpLayout;
 	QGroupBox *dirBox, *fileTypeBox;
 	QTableView *dirTableView, *fileTypeTableView;
-	QPushButton *dirAddBtn, *dirRemoveBtn;
+	QStandardItemModel *dirModel;
+	QPushButton *dirAddBtn, *dirRemoveBtn, *fileTypeAddBtn, *fileTypeRemoveBtn;
+
+	/* 右侧布局使用的控件 */
+	QVBoxLayout *rightLayout, *resultLayout;
+	QHBoxLayout *resultOpLayout;
+	QGroupBox *resultBox;
+	QTableView *resultTableView;
+	QStandardItemModel *resultModel;
+	QPushButton *startBtn;
+
+public slots:
+	void dirAddBtnClicked();
 };
 
 #endif // MAINWINDOW_H
